3f0710e872af1d3670b1cdebcc9eef45bebdce07
Overdr0ne <0v3rdr0n3@gmail.com>
2021-03-30 23:59:14 -0400

    initial commit

---
+++ shellout.el
@@ -0,0 +1,15 @@
+(defvar shelldon-nth 0)
+(defvar shelldon-hist '())
+(defun shelldon (command &optional output-buffer error-buffer)
+  (interactive "sEnter command: ")
+  (setq output-buffer (concat " *" (number-to-string shelldon-nth) ":" command "*"))
+  (setq shelldon-nth (+ shelldon-nth 1))
+  (add-to-list 'shelldon-hist `(,(concat (number-to-string shelldon-nth) ":" command) . ,output-buffer))
+  (shell-command command output-buffer error-buffer)
+  (with-current-buffer output-buffer (buffer-string)))
+
+(add-to-list 'ivy-sort-functions-alist '(shelldon-hist- . string<))
+
+(defun shelldon-hist ()
+  (interactive)
+  (switch-to-buffer (cdr (assoc (completing-read ">> " shelldon-hist) shelldon-hist))))
