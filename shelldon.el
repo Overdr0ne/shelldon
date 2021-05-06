b1a102e57e9c26392a879718b52c26b69834560b
Overdr0ne <0v3rdr0n3@gmail.com>
2021-05-06 10:33:12 -0600

    add standard elisp header template

--- shelldon.el
+++ shelldon.el
@@ -1,3 +1,32 @@
+;;; shelldon.el --- a friendly little shell in the minibuffer  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 overdr0ne
+
+;; Author: overdr0ne <scmorris.dev@gmail.com>
+;; Keywords: tools, convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; https://github.com/Overdr0ne/shelldon
+;; It is basically just a simple wrapper around async-shell-command that
+;; primarily allows you to store and navigate separate command outputs among
+;; other things.
+
+;;; Code:
+
 (require 'cl-macs)
 
 (defvar shelldon-hist '())
@@ -35,3 +64,5 @@
   (switch-to-buffer (cdr (assoc (completing-read ">> " shelldon-hist) shelldon-hist))))
 
 (provide 'shelldon)
+
+;;; shelldon.el ends here
