9afdbdb4ac0edbb7f8e823bf00a46f16eb123416
Overdr0ne <0v3rdr0n3@gmail.com>
2021-05-17 14:55:53 -0600

    add an explicit default windowing configuration

--- shelldon.el
+++ shelldon.el
@@ -99,6 +99,14 @@ the change and re-execute in the new context."
 (defalias 'shelldon-hist 'shelldon-output-history
   "shelldon-hist is deprecated, use shelldon-output-history")
 
+(add-to-list 'display-buffer-alist
+	     `("*\\(shelldon.*\\)"
+	       (display-buffer-reuse-window display-buffer-in-previous-window display-buffer-in-side-window)
+	       (side . right)
+	       (slot . 0)
+	       (window-width . 80)
+	       (reusable-frames . visible)))
+
 (provide 'shelldon)
 
 ;;; shelldon.el ends here
