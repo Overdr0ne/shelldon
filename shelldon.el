061b20019bd2212b7f6008874a90e18cdf5a6cf3
Overdr0ne <0v3rdr0n3@gmail.com>
2021-06-01 18:15:26 -0600

    allow ANSI colors and explain how to enable in readme
    This required setting the TERM variable to eterm-color among other
    things. In order not to set this globally, shell-command had to be
    modified to set a local process-environment. So I just copied it, and
    removed a ton of dead code paths.

--- shelldon.el
+++ shelldon.el
@@ -43,36 +43,84 @@ context."
 
 (defvar shelldon-hist '())
 (defvar shelldon-prompt-str ">> ")
-(defun shelldon-internal (command &optional output-buffer error-buffer)
-  "Execute COMMAND asynchronously in the minibuffer with output history.
+(defun shelldon-internal (command)
+  "Execute string COMMAND in inferior shell; display output, if any.
+With prefix argument, insert the COMMAND's output at point.
 
-Keep track of each command output in a separate buffer.  Optionally send stdout
-to OUTPUT-BUFFER and stderr to ERROR-BUFFER, just like the raw
-’async-shell-command’."
+In Elisp, you will often be better served by calling `call-process' or
+`start-process' directly, since they offer more control and do not
+impose the use of a shell (with its need to quote arguments)."
   (interactive
    (list
-    (read-shell-command (if shell-command-prompt-show-cwd
-                            (format-message "%s%s"
-                                            (abbreviate-file-name
-                                             default-directory)
-                                            shelldon-prompt-str)
-                          shelldon-prompt-str)
-                        nil nil
-                        (let ((filename
-                               (cond
-                                (buffer-file-name)
-                                ((eq major-mode 'dired-mode)
-                                 (dired-get-filename nil t)))))
-                          (and filename (file-relative-name filename))))
-    current-prefix-arg
-    shell-command-default-error-buffer))
-  (unless (string-match "&[ \t]*\\'" command)
-    (setq command (concat command " &")))
-  (setq output-buffer (concat " *shelldon:" (number-to-string (length shelldon-hist)) ":" command "*"))
-  (add-to-list 'shelldon-hist `(,(concat (number-to-string (length shelldon-hist)) ":" command) .
-                                ,output-buffer))
-  (shell-command command output-buffer error-buffer)
-  (with-current-buffer output-buffer (view-mode))
+    (read-shell-command
+     (if shell-command-prompt-show-cwd
+         (format-message "%s%s"
+                         (abbreviate-file-name
+                          default-directory)
+                         shelldon-prompt-str)
+       shelldon-prompt-str)
+     nil nil
+     (let ((filename
+            (cond
+             (buffer-file-name)
+             ((eq major-mode 'dired-mode)
+              (dired-get-filename nil t)))))
+       (and filename (file-relative-name filename))))))
+  ;; (when current-prefix-arg (setq output-buffer current-prefix-arg))
+  ;; Look for a handler in case default-directory is a remote file name.
+  (let ((output-buffer (concat "*shelldon:" (number-to-string (length shelldon-hist)) ":" command "*"))
+        (error-buffer shell-command-default-error-buffer)
+        (handler
+	 (find-file-name-handler (directory-file-name default-directory)
+				 'shell-command)))
+    (add-to-list 'shelldon-hist `(,(concat (number-to-string (length shelldon-hist)) ":" command) . ,output-buffer))
+    (if handler
+	(funcall handler 'shell-command command output-buffer error-buffer)
+      ;; Output goes in a separate buffer.
+      ;; Preserve the match data in case called from a program.
+      ;; FIXME: It'd be ridiculous for an Elisp function to call
+      ;; shell-command and assume that it won't mess the match-data!
+      (save-match-data
+        (let* ((buffer (get-buffer-create output-buffer))
+               (proc (get-buffer-process buffer))
+               (directory default-directory))
+	  (with-current-buffer buffer
+            (shell-command-save-pos-or-erase)
+	    (setq default-directory directory)
+	    (let* ((process-environment
+                    (nconc
+                     (list
+                      (format "TERM=%s" "eterm-color")
+                      (format "TERMINFO=%s" data-directory)
+                      (format "INSIDE_EMACS=%s" emacs-version))
+                     process-environment)))
+	      (setq proc
+		    (start-process-shell-command "Shell" buffer command)))
+	    (setq mode-line-process '(":%s"))
+	    (require 'shell) (shell-mode)
+            (set-process-sentinel proc #'shell-command-sentinel)
+	    ;; Use the comint filter for proper handling of
+	    ;; carriage motion (see comint-inhibit-carriage-motion).
+            (set-process-filter proc #'comint-output-filter)
+            (if async-shell-command-display-buffer
+                ;; Display buffer immediately.
+                (display-buffer buffer '(nil (allow-no-window . t)))
+              ;; Defer displaying buffer until first process output.
+              ;; Use disposable named advice so that the buffer is
+              ;; displayed at most once per process lifetime.
+              (let ((nonce (make-symbol "nonce")))
+                (add-function :before (process-filter proc)
+                              (lambda (proc _string)
+                                (let ((buf (process-buffer proc)))
+                                  (when (buffer-live-p buf)
+                                    (remove-function (process-filter proc)
+                                                     nonce)
+                                    (display-buffer buf))))
+                              `((name . ,nonce)))))
+            ;; FIXME: When the output buffer is hidden before the shell process is started,
+            ;; ANSI colors are not displayed. I have no idea why.
+            (view-mode)
+            (rename-buffer (concat " " output-buffer)))))))
   nil)
 (defun shelldon ()
   "Execute given asynchronously in the minibuffer with output history.
